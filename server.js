9a0bbc0a57d99742c0bac95623389f8aab85b580
Natasa <nkilibarda3@gmail.com>
2023-06-13 17:14:57 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
